515f8958c6e3c0077cdb5f2050ec074d0b487fcc
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-05-12 15:11:49 +0900

    Added gitignore and removed cached files

--- out/extension.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const catScratchEditor_1 = require("./catScratchEditor");
-function activate(context) {
-    // Register our custom editor providers
-    context.subscriptions.push(catScratchEditor_1.CatScratchEditorProvider.register(context));
-}
-exports.activate = activate;
-//# sourceMappingURL=extension.js.map
