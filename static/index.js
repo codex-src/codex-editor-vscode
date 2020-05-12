b21245f37ef1fc9b4292e25f809c0c369dc3401f
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-05-12 19:10:59 +0900

    More simplification

--- static/index.js
+++ static/index.js
@@ -1,46 +1,17 @@
-// ;(() => {
-// 	// Get the VSCode API and document state:
-// 	const vscode = acquireVsCodeApi()
-// 	const documentState = vscode.getState()
-//
-// 	const textarea = document.querySelector("textarea")
-// 	if (documentState) {
-// 		textarea.value = state.text // TODO
-// 	}
-//
-// 	// Sends a message to VSCode.
-// 	textarea.addEventListener("input", e => {
-// 		const text = e.target.value
-// 		vscode.postMessage({
-// 			type:, "input",
-// 			text,
-// 		})
-// 	})
-//
-// 	// Consumes a message from VSCode.
-// 	window.addEventListener("message", e => {
-// 		const message = e.data
-// 		textarea.value = message.text // TODO
-//
-// 		// TODO: Remove?
-// 		vscode.setState({ text: message.text })
-// 	})
-// })()
-
-(() => {
+;(() => {
 	const vscode = acquireVsCodeApi()
-	const state = vscode.getState()
+	const initialState = vscode.getState()
 
 	const textarea = document.querySelector("textarea")
-	if (state) {
-		textarea.value = state.text // TODO
+	if (initialState) {
+		textarea.value = initialState.text // TODO
 	}
 
 	// Sends messages to VSCode.
 	textarea.addEventListener("input", e => {
 		const text = e.target.value
 		vscode.postMessage({
-			type: "input", // TODO: Can remove
+			type: "input",
 			text,
 		})
 	})
